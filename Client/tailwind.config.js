64481b725a2fd3f74775aeedc5c1603f341fdff5
Martin Skjønhaug <martin@alv.no>
2023-10-21 12:52:36 +0200

    Fixed typo in tailwindconfig

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{razor, html}"],
+  content: ["./**/*.{html,razor}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
